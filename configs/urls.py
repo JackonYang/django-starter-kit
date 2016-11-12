85aad45ca05e8b52e796225c5dc2fa978cc2018c
Jackon Yang <i@jackon.me>
2016-11-12 15:44:36 +0800

    grappelli

--- configs/urls.py
+++ configs/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from . import views
@@ -22,5 +22,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
 ]
