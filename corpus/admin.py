a153ef6e9e4d9752f1f25e2e601f7e5107b91c8a
Jackon Yang <i@jackon.me>
2016-12-14 23:01:13 +0800

    show tag in admin page

--- corpus/admin.py
+++ corpus/admin.py
@@ -2,8 +2,16 @@
 from django.contrib import admin
 
 from .models import (
+    Tag,
     UserText,
 )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name',
+    )
 
 
 @admin.register(UserText)
